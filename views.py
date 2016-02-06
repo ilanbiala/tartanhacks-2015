90a8a548f3c2a40b48b107d110ef70f4bc6f2191
jin03002 <kimildoo12@gmail.com>
2016-02-05 22:18:53 -0500

    Added Postgresql code

--- views.py
+++ views.py
@@ -1,21 +1,47 @@
 from flask import *
+import psycopg2
 from server import *
+import base64
+import sys
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('home.html')
+conn = psycopg2.connect(dbname = 'postgres', user = 'postgres', host = 'localhost', password='postgres')
+sql = conn.cursor()
+# sql.execute("CREATE TABLE andrewUsers(id VARCHAR(20) PRIMARY KEY, password VARCHAR(20))")
+# conn.commit()
 
-@app.route('/register', methods=['get','post'])
+@app.route('/login', methods = ['get','post'])
+def login():
+	if request.method == 'POST':
+		user_id = (request.form['username'])
+		user_pw = (request.form['password'])
+		query = "SELECT * FROM andrewUsers where id = '%s'" % user_id
+		sql.execute(query)
+		user = sql.fetchall()
+		encrypted_pw = user[0][1]
+		decrypted_pw = (base64.b64decode(encrypted_pw).decode('utf8'))
+		if user_pw == decrypted_pw:
+			print("Correct Password", sys.stderr)
+
+	return render_template('login.html')
+
+@app.route('/register', methods = ['get','post'])
 def register():
-    if request.method=='POST':
-    	user_id=(request.form['username'])
-    	user_pw=(request.form['password'])
-    	user_pw_re=(request.form['password_re'])
-    	do_something_with_register()
-    	
-    return render_template('register.html')
+	if request.method == 'POST':
+		user_id = (request.form['username'])
+		user_pw = (request.form['password'])
+		user_pw_re = (request.form['password_re'])
 
-if __name__=='__main__':
+		if(user_pw == user_pw_re):
+			encrypted_pw = base64.b64encode(bytes(user_pw, 'utf8')).decode('utf8')
+			print("Password Matching %s" %encrypted_pw, type(encrypted_pw), file=sys.stderr)
+			query = "INSERT INTO andrewUsers (id, password) VALUES (%s,%s)"
+			data  = (user_id, encrypted_pw)
+			sql.execute(query,data)
+			conn.commit()
+
+	return render_template('register.html')
+
+if __name__ == '__main__':
 	app.debug = True
 	app.run()
