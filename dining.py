244b9c03f025e51ebd0c5e567937bf30300c0306
Rishub Jain <rishubjain@gmail.com>
2016-02-05 21:22:27 -0500

    finished dining

--- dining.py
+++ dining.py
@@ -1,5 +1,34 @@
 import requests
+import json
 
 r = requests.get('http://apis.scottylabs.org/dining/v1/locations')
 
 d = r.json()
+
+# 0 is Sunday, 6 is Saturday
+
+def restaurants_available(day, starth, startm, endh, endm):
+	rests = []
+	for x in d["locations"]:
+		for s in x["times"]:
+
+			# TODO: return the intervals for which the restaurants are open
+			# This only returns the restaurants open for the entire duration
+			if ((s["start"]["day"] ==day and s["end"]["day"] == day and
+				(starth>s["start"]["hour"] or (starth==s["start"]["hour"] and startm>=s["start"]["min"])) and
+				(endh<s["end"]["hour"] or (endh==s["end"]["hour"] and endm<=s["end"]["min"]))) or
+
+				(s["start"]["day"] < day and s["end"]["day"] ==day and
+				(endh<s["end"]["hour"] or (endh==s["end"]["hour"] and endm<=s["end"]["min"]))) or
+
+				(s["start"]["day"] == day and s["end"]["day"] > day and
+				(starth>s["start"]["hour"] or (starth==s["start"]["hour"] and startm>=s["start"]["min"]))) or
+
+				(s["start"]["day"] < day and s["end"]["day"] > day)):
+				# rests.append(x["name"].endcode('utf-8'))
+				rests.append(x["name"].encode('utf-8'))
+	s = list(set(rests))
+	print s
+	return s
+
+restaurants_available(5,20,01,21,02)
