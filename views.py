4187577ad585509b10f9b71a93ee7d274226fdd7
Rishub Jain <rishubjain@gmail.com>
2016-02-06 13:35:27 -0500

    fixed freetime, added same classes

--- views.py
+++ views.py
@@ -2,11 +2,14 @@ from flask import *
 import psycopg2
 from server import *
 from courses import *
+import freetime
 import base64
 import sys
 import urllib.parse
 app = Flask(__name__)
 
+my_id= 'ildook'
+my_pw= 'Jin03002!'
 urllib.parse.uses_netloc.append("postgres")
 url = urllib.parse.urlparse('postgres://zqirlutztjyaov:ceb3bv_TL8S_9KjggKchociRsN@ec2-54-225-199-245.compute-1.amazonaws.com:5432/d7193dajm703tu')
 
@@ -17,9 +20,10 @@ conn = psycopg2.connect(
     host=url.hostname,
     port=url.port
 )
-# sql = conn.cursor()
-# sql.execute("CREATE TABLE andrewUsers(id VARCHAR(20) PRIMARY KEY, password VARCHAR(20), classes VARCHAR(10000))")
-# conn.commit()
+sql = conn.cursor()
+
+#sql.execute("CREATE TABLE andrewUsers(id VARCHAR(20) PRIMARY KEY, firstname VARCHAR(20), lastname VARCHAR(20), fullname VARCHAR(41), password VARCHAR(20), classes VARCHAR(100000))")
+#conn.commit()
 
 @app.route('/calendar', methods=['get','post'])
 def calendar():
@@ -29,11 +33,22 @@ def calendar():
 		query = "SELECT * FROM andrewUsers where id = '%s'" % user_id
 		sql.execute(query)
 		user = sql.fetchall()
-		encrypted_pw = user[0][1]
+		encrypted_pw = user[0][4]
 		decrypted_pw = (base64.b64decode(encrypted_pw).decode('utf8'))
 		if user_pw == decrypted_pw:
 			#print("Correct Password", file=sys.stderr)
-			return render_template('calendar.html')
+			query = "SELECT * FROM andrewUsers"
+			sql.execute(query)
+			users_all = sql.fetchall()
+			
+			user_dict={}
+			for cuser in users_all:
+				user_dict[cuser[3]]=cuser[5]
+
+			free_time=freetime(user_dict, user[0][5])
+
+			return render_template('calendar.html', freetime=free_time)
+			
 		else:
 			return render_template('index.html', password_match = 'false')
 
@@ -45,19 +60,19 @@ def index():
 		user_id = (request.form['username'])
 		user_pw = (request.form['password'])
 		user_pw_re = (request.form['password_re'])
-
 		if(user_pw == user_pw_re):
-			courses = get_courses(user_id, user_pw)
-			encrypted_pw = base64.b64encode(bytes(user_pw, 'utf8')).decode('utf8')
-			#print("Password Matching %s" %encrypted_pw, type(encrypted_pw), file=sys.stderr)
-			query = "INSERT INTO andrewUsers (id, password) VALUES (%s,%s)"
-			data  = (user_id, encrypted_pw)
-			sql.execute(query,data)
-			conn.commit()
-			return render_template('index.html', password_match = 'true')
+			courses = get_courses(my_id, my_pw)
+			print(courses, file=sys.stderr)
+			if(courses!=None):
+				encrypted_pw = base64.b64encode(bytes(user_pw, 'utf8')).decode('utf8')
+				#print("Password Matching %s" %encrypted_pw, type(encrypted_pw), file=sys.stderr)
+				query = "INSERT INTO andrewUsers (id, password, classes) VALUES (%s,%s,%s)"
+				data  = (user_id, encrypted_pw, courses)
+				sql.execute(query,data)
+				conn.commit()
+				return render_template('index.html', password_match = 'true')
 
-		else:
-			return render_template('index.html', password_match = 'false')
+		return render_template('index.html', password_match = 'false')
 
 	return render_template('index.html')
 
