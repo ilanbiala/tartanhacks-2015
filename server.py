457587d9cbfd00fd544cb4d49d2ff0cc2bed2473
jin03002 <kimildoo12@gmail.com>
2016-02-05 20:26:49 -0500

    Initial Setup

--- server.py
+++ server.py
@@ -1,4 +1,1 @@
 #!/usr/bin/env python
-from tartanhacks import tartanhacks
-
-tartanhacks.run(debug = True)
--- views.py
+++ views.py
@@ -1,6 +1,21 @@
-from flask import render_template
-from tartanhacks import tartanhacks
+from flask import *
+from server import *
+app = Flask(__name__)
 
-@tartanhacks.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/register', methods=['get','post'])
+def register():
+    if request.method=='POST':
+    	user_id=(request.form['username'])
+    	user_pw=(request.form['password'])
+    	user_pw_re=(request.form['password_re'])
+    	do_something_with_register()
+    	
+    return render_template('register.html')
+
+if __name__=='__main__':
+	app.debug = True
+	app.run()
